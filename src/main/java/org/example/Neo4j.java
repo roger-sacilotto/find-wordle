b68b0a8810393cde17f3479096eaae286c14b926
Roger Sacilotto <roger.sacilotto@avid.com>
2022-01-19 11:21:50 -0500

    refactor authorization

--- src/main/java/org/example/Neo4j.java
+++ src/main/java/org/example/Neo4j.java
@@ -5,11 +5,13 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import java.util.Base64;
+
 public class Neo4j {
     public static JsonNode cypherTx(String query) throws UnirestException {
-
+        String auth = Base64.getEncoder().encodeToString(String.format("%s:%s", Constants.NEO4J_USER, Constants.NEO4J_PASSWORD).getBytes());
         HttpResponse<JsonNode> jsonResponse = Unirest.post("http://localhost:7474/db/neo4j/tx/commit").header("Content-Type", "application/json")
-                .header("Accept", "application/json;charset=UTF-8").header("Authorization", "Basic bmVvNGo6QXZpZDEyMw==")
+                .header("Accept", "application/json;charset=UTF-8").header("Authorization",  String.format("Basic %s", auth))
                 .body(query).asJson();
         JsonNode body = jsonResponse.getBody();
         return body;
